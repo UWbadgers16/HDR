1d5e906f4dbba8df9a6161e6853a91f02a89d3d8
John <cabaj@wisc.edu>
2015-02-16 20:18:34 -0600

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Globalization;
+using Windows.UI.Popups;
+using Windows.Media.MediaProperties;
+using Windows.Media.Devices;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace HDR
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		MediaCapture mediaCapture;
+        ImageEncodingProperties imageEncoding;
+		DeviceInformation deviceInformation;
+		MessageDialog messageDialog;
+        FocusSettings focusSettings;
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+
+			this.NavigationCacheMode = NavigationCacheMode.Required;
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected async override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			// TODO: Prepare page for display here.
+
+			// TODO: If your application contains multiple pages, ensure that you are
+			// handling the hardware Back button by registering for the
+			// Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+			// If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            mediaCapture = new MediaCapture();
+            deviceInformation = await GetCameraDeviceInfoAsync(Windows.Devices.Enumeration.Panel.Back);
+
+            var settings = new MediaCaptureInitializationSettings();
+            settings.PhotoCaptureSource = PhotoCaptureSource.VideoPreview;
+            if (deviceInformation != null)
+                settings.VideoDeviceId = deviceInformation.Id;
+
+            await mediaCapture.InitializeAsync(settings);
+            mediaCapture.VideoDeviceController.PrimaryUse = CaptureUse.Photo;
+
+            imageEncoding = ImageEncodingProperties.CreateJpeg();
+
+            focusSettings = new FocusSettings();
+            focusSettings.AutoFocusRange = AutoFocusRange.FullRange;
+            focusSettings.Mode = FocusMode.Auto;
+            focusSettings.WaitForFocus = true;
+            focusSettings.DisableDriverFallback = false;
+            mediaCapture.VideoDeviceController.FocusControl.Configure(focusSettings);
+
+            mediaCapture.VideoDeviceController.FlashControl.Enabled = false;
+
+            if (!mediaCapture.VideoDeviceController.ExposureControl.Supported)
+            {
+                messageDialog = new MessageDialog("Exposure control is not supported!");
+                await messageDialog.ShowAsync();
+            }
+
+            previewElement.Source = mediaCapture;
+            mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
+            await mediaCapture.StartPreviewAsync();
+		}
+
+		private async void captureButton_Click(object sender, RoutedEventArgs e)
+        {
+            await mediaCapture.VideoDeviceController.FocusControl.FocusAsync();
+            await mediaCapture.VideoDeviceController.ExposureCompensationControl.SetValueAsync(mediaCapture.VideoDeviceController.ExposureCompensationControl.Min);
+            var photoStorageFile = await KnownFolders.CameraRoll.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+            await mediaCapture.CapturePhotoToStorageFileAsync(imageEncoding, photoStorageFile);
+            SaveImage(photoStorageFile);
+
+            await mediaCapture.VideoDeviceController.ExposureCompensationControl.SetValueAsync(0);
+            photoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+            await mediaCapture.CapturePhotoToStorageFileAsync(imageEncoding, photoStorageFile);
+            SaveImage(photoStorageFile);
+
+            await mediaCapture.VideoDeviceController.ExposureCompensationControl.SetValueAsync(mediaCapture.VideoDeviceController.ExposureCompensationControl.Max);
+            photoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+            await mediaCapture.CapturePhotoToStorageFileAsync(imageEncoding, photoStorageFile);
+            SaveImage(photoStorageFile);
+
+            await mediaCapture.VideoDeviceController.ExposureCompensationControl.SetValueAsync(0);
+		}
+
+        private static async Task<DeviceInformation> GetCameraDeviceInfoAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            DeviceInformation device = (await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture))
+                .FirstOrDefault(d => d.EnclosureLocation != null && d.EnclosureLocation.Panel == desiredPanel);
+
+            if (device == null)
+            {
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "No suitable devices found for the camera of type {0}.", desiredPanel));
+            }
+
+            return device;
+        }
+
+        private async void SaveImage(StorageFile file)
+        {
+            var fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+            var bitmapImage = new BitmapImage();
+            await bitmapImage.SetSourceAsync(fileStream);
+            var decoder = await BitmapDecoder.CreateAsync(fileStream);
+
+            var memStream = new InMemoryRandomAccessStream();
+            var encoder = await BitmapEncoder.CreateForTranscodingAsync(memStream, decoder);
+
+            encoder.BitmapTransform.Rotation = BitmapRotation.Clockwise90Degrees;
+
+            try
+            {
+                await encoder.FlushAsync();
+            }
+            catch (Exception err)
+            {
+                switch (err.HResult)
+                {
+                    case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
+                        // If the encoder does not support writing a thumbnail, then try again
+                        // but disable thumbnail generation.
+                        encoder.IsThumbnailGenerated = false;
+                        break;
+                    default:
+                        throw err;
+                }
+            }
+
+            memStream.Seek(0);
+            fileStream.Seek(0);
+            fileStream.Size = 0;
+            await RandomAccessStream.CopyAsync(memStream, fileStream);
+
+            fileStream.Dispose();
+            memStream.Dispose();
+        }
+	}
+}
